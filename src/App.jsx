be6c313c9c1095f89ab2247e64b61b1193690a10
aadil <salmanaadil52@gmail.com>
2025-08-20 12:59:04 +0500

    Navbar fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./sections/navbar";
+\
 import Hero from "./sections/Hero";
 import About from "./sections/About";
 import Projects from "./sections/Projects";
@@ -7,6 +7,7 @@ import Experiences from "./sections/Experiences";
 import Testimonial from "./sections/Testimonial";
 import Contact from "./sections/Contact";
 import Footer from './sections/Footer';
+import Navbar from "./sections/Navbar";
 
 const App = () => {
   return (
